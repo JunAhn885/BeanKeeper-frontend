66d4b96cc8ee0224cbb23196746de0a407f6af57
Jun Ahn <junahn@Juns-MacBook-Air-4.local>
2023-05-03 21:58:10 -0700

    s

--- frontend/src/components/bondingJournal/gratitudeBox.js
+++
@@ -1,9 +0,0 @@
-const GratitudeBox = (props) => {
-    return (
-        <div className="bonding-journal-gratitude-box">
-            <h1>{props.content}</h1>
-        </div>
-    )
-}
-
-export default GratitudeBox;
